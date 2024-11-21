c845af30d7c57b247e9ed3bb5e826c52f2cd06c1
Eslam Samy <eslamsamy226@yahoo.com>
2024-11-21 14:29:20 +0200

    added the initial setup for the backend and the frontend

---
+++ timease-backend/src/main/java/com/timease/backend/TimeaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.timease.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeaseBackendApplication.class, args);
+	}
+
+}
---
+++ timease-backend/src/test/java/com/timease/backend/TimeaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timease.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
