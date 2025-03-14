e48797ed64b6cde143748bb01313082330c67f16
Elsayed Ahmed <93007715+sayedmaltan@users.noreply.github.com>
2025-03-14 03:37:05 +0200

    feat/update event

--- timease_mobile/lib/features/event/presentation/views/widgets/custom_event_container.dart
+++ timease_mobile/lib/features/event/presentation/views/widgets/custom_event_container.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:timease_mobile/constants.dart';
-import 'package:timease_mobile/core/utils/app_router.dart';
 import 'package:timease_mobile/features/event/data/models/event_model.dart';
 import '../../../../../core/utils/function/show_event_bottom_sheet.dart';
 import '../../../../../core/utils/styles.dart';
