215a25ba4d986a2e089e7892b7f1aeb7230b7d9a
Elsayed Ahmed <93007715+sayedmaltan@users.noreply.github.com>
2025-05-07 01:15:31 +0300

    add notification widget

--- timease_mobile/lib/features/notification/presentation/views/notifications_screen_view_body.dart
+++ timease_mobile/lib/features/notification/presentation/views/notifications_screen_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:timease_mobile/core/utils/styles.dart';
+import 'package:timease_mobile/features/notification/presentation/views/widgets/notification_card.dart';
 import '../../../../core/utils/function/logout.dart';
 
 class NotificationsScreenViewBody extends StatelessWidget {
@@ -6,13 +8,39 @@ class NotificationsScreenViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        onPressed: () async {
-          logout(context: context);
-        },
-        child: Text('LOGOUT'),
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 14,
+      children: [
+        Row(
+          children: [
+            SizedBox(
+              width: 18,
+            ),
+            Text(
+              'Notifications',
+              style: Styles.textFormField.copyWith(
+                color: Colors.black,
+                fontSize: 26,
+              ),
+            ),
+          ],
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: 2,
+            itemBuilder: (context, index) {
+              return NotificationCard();
+            },
+          ),
+        ),
+        TextButton(
+          onPressed: () async {
+            logout(context: context);
+          },
+          child: Text('LOGOUT'),
+        ),
+      ],
     );
   }
 }
